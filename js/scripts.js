398f9df8e40d444c90020b2907716b619bbd7760
dbins <bins@ig.com.br>
2015-11-08 10:50:46 -0300

    2015-11-07

--- js/scripts.js
+++ js/scripts.js
@@ -609,3 +609,9 @@
 			}, "JSONP");	
 		
 		});	
+		
+		$( document ).bind( "mobileinit", function() {
+		  // Make your jQuery Mobile framework configuration changes here!
+
+		  $.mobile.allowCrossDomainPages = true;
+		});
