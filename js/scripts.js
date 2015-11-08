403047b465c4ef8b2ba33acc576746e8b694cbef
dbins <bins@ig.com.br>
2015-11-08 11:02:19 -0300

    2015-11-07

--- js/scripts.js
+++ js/scripts.js
@@ -612,6 +612,6 @@
 		
 		$( document ).bind( "mobileinit", function() {
 		  // Make your jQuery Mobile framework configuration changes here!
-
+		  $.support.cors = true;
 		  $.mobile.allowCrossDomainPages = true;
 		});
